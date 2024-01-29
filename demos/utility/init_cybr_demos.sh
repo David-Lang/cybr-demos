5f900a7049e9c566e58577e03c22a5c983b6a3bb
David-Lang <16465823+David-Lang@users.noreply.github.com>
2024-01-29 11:53:52 -0500

    update

--- demos/utility/init_cybr_demos.sh
+++ demos/utility/init_cybr_demos.sh
@@ -6,4 +6,5 @@ rm -rf /home/ubuntu/cybr-demos
 git clone https://github.com/David-Lang/cybr-demos.git \
   --branch $branch --depth 1 --single-branch
 
-"$CYBR_DEMOS_PATH"/demos/utility/reset_cybr_demos.sh
+cybr-demos/compute_init/ubuntu/setup.sh
+cybr-demos/demos/utility/overwrite_vars.env.sh
