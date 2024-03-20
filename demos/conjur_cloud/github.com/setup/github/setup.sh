1e6318ff226e19238abd80b99511ee536f33f706
David-Lang <16465823+David-Lang@users.noreply.github.com>
2024-03-20 19:30:04 -0400

    update

--- demos/conjur_cloud/github.com/setup/github/setup.sh
+++ demos/conjur_cloud/github.com/setup/github/setup.sh
@@ -7,17 +7,18 @@ source "$CYBR_DEMOS_PATH/demos/isp_vars.env.sh"
 main() {
   set_variables
 
-  printf "\n\nIn the github repo to be used configure these variables:\n"
-
   printf "\n\nresolve_template workload1.tmpl.yaml workload1.yaml\n"
   resolve_template "settings_variables.tmpl.env" "settings_variables.env"
 
+  printf "\n\nIn the github repo to be used configure these variables:\n"
   cat settings_variables.env
 }
 
 # shellcheck disable=SC2153
+# shellcheck disable=SC2034
 set_variables() {
   printf "\nSetting local vars from Env"
+
   isp_id=$TENANT_ID
   isp_subdomain=$TENANT_SUBDOMAIN
   safe_name=$SAFE_NAME
