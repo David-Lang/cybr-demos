bfcd3129c2d36f037fb15fb10094beab603be685
David-Lang <16465823+David-Lang@users.noreply.github.com>
2024-03-20 16:21:50 -0400

    update

--- demos/isp_vars.env.sh
+++ demos/isp_vars.env.sh
@@ -5,8 +5,9 @@
 # Using ‘+’ rather than ‘-’ causes the option to be turned off
 set -a
 source "$CYBR_DEMOS_PATH/demos/isp_vars.env"
-source "$CYBR_DEMOS_PATH/demos/isp_functions/conjur_functions"
-source "$CYBR_DEMOS_PATH/demos/isp_functions/vault_functions"
+source "$CYBR_DEMOS_PATH/demos/isp_functions/identity_functions.sh"
+source "$CYBR_DEMOS_PATH/demos/isp_functions/conjur_functions.sh"
+source "$CYBR_DEMOS_PATH/demos/isp_functions/vault_functions.sh"
 set +a
 
 is_tool_installed() {
