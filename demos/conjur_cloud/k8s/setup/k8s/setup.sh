3b8de4a1e2e690623e2c98b26bc5f96b8a920ce4
David-Lang <16465823+David-Lang@users.noreply.github.com>
2025-12-09 15:48:53 -0800

    update

--- demos/conjur_cloud/k8s/setup/k8s/setup.sh
+++ demos/conjur_cloud/k8s/setup/k8s/setup.sh
@@ -10,12 +10,14 @@ source "$demo_path/setup/vars.env"
 set +a
 
 sm_fqdn="$SM_FQDN"
-sm_namespace="$NAMESPACE"
+sm_service_name="$SM_SERVICE_NAME"
+
+sm_secret_1_id="$SM_SECRET_1_ID"
+sm_secret_1_name="$SM_SECRET_1_NAME"
+sm_secret_2_id="$SM_SECRET_2_ID"
+sm_secret_2_name="$SM_SECRET_2_NAME"
 
 # Setup k8s
-
-openssl s_client -connect "$sm_fqdn":443 </dev/null 2>/dev/null \
-| openssl x509 -inform pem -text > sm.pem
 
 # Setup IAM EKS Admin creds
 # eks_name="<-- set" # poc-cdn
@@ -26,10 +28,8 @@ openssl s_client -connect "$sm_fqdn":443 </dev/null 2>/dev/null \
 
 # Helm install ESO Service
 
-
 kubectl get crd externalsecrets.external-secrets.io secretstores.external-secrets.io \
   -o custom-columns=NAME:.metadata.name,VERSIONS:.spec.versions[*].name
-
 
 helm repo add external-secrets https://charts.external-secrets.io
 helm repo update
@@ -48,19 +48,21 @@ kubectl -n external-secrets get pods
 # ESO CRD
 kubectl apply -f https://raw.githubusercontent.com/external-secrets/external-secrets/main/deploy/crds/bundle.yaml
 
+openssl s_client -connect "$sm_fqdn":443 </dev/null 2>/dev/null \
+| openssl x509 -inform pem -text > sm.pem
 
 # Helm install Release names does not allow '_' use '-'
 helm install poc-sm-k8s \
      charts/poc-sm \
      --namespace default \
-     --set namespace=$sm_namespace \
+     --set namespace=$sm_service_name \
      --set sm_fqdn="$sm_fqdn" \
      --set sm_cert_b64="$(cat sm.pem | base64 -w0 )" \
-     --set sm_authn_id="poc-sm-k8s-1" \
+     --set sm_authn_id=$sm_service_name \
      --set sm_app_service_account="poc-service-account" \
-     --set sm_secret_1_id="data/vault/safe/account/username" \
-     --set sm_secret_1_name="username" \
-     --set sm_secret_2_id="data/vault/safe/account/password" \
-     --set sm_secret_2_name="password" \
+     --set sm_secret_1_id="$sm_secret_1_id" \
+     --set sm_secret_1_name="$sm_secret_1_name" \
+     --set sm_secret_2_id="$sm_secret_2_id" \
+     --set sm_secret_2_name="$sm_secret_2_name" \
      --debug
 
